c767050c1a504a53128ce95331397b2ca9e5ccc2
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-11-29 19:50:46 +0100

    Refactored code for improvements.

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class MainApplication {
     private static Diary diary;
     public static void main(String[] args) {
@@ -7,34 +6,38 @@ public class MainApplication {
         Scanner input = new Scanner(System.in);
         System.out.println("""
                 Welcome to diary app! Start by introducing yourself. Happy journaling!
-                
+                                
                 1  ->  Create Diary
-                2  ->  Unlock Diary
-                3  ->  Find Entry by Id
+                2  ->  Lock Diary
+                3  ->  Unlock Diary
                 4  ->  Add Entry
-                5  ->  Delete Entry
-                6  ->  Update Entry
-                7  ->  Lock Diary
+                5  ->  Find Entry by Id
+                6  ->  Delete Entry
+                7  ->  Update Entry
                 """);
-
-        char userInput = 'A';
-
-        switch (userInput){
-            case '1' -> {
-                System.out.print("Choose a username for your new account: ");
+        char userInput = input.next().charAt(0);
+        switch (userInput) {
+            case '1':
+                System.out.print("""
+                        1  ->  Create Diary
+                                                
+                        Choose a username for your new account:
+                        """);
                 String username = input.nextLine();
 
                 System.out.print("Now, let's set up a password. For security, choose a secure password with letters, numbers, and special characters: ");
                 String password = input.nextLine();
 
-                diary = new Diary("thetosinfaith","4040");
+                diary = new Diary("thetosinfaith", "4040");
 
                 break;
             }
+             case '2' -> {
+                    System.out.print("""
+                            2  ->  Unlock Diary
 
-            switch (userInput){
-                case '2' ->{
-                    System.out.print("Sure, to unlock, what's your diary password? ");
+                            Sure, to unlock, what's your diary password?          
+                            """);
                     String password = input.nextLine();
 
                     diary.unlockDiary("4040");
@@ -42,138 +45,78 @@ public class MainApplication {
                     break;
                 }
 
+                default -> {
 
-                "And could you enter your account's password to unlock it?"
+                    int password = 4040;
 
+                    if (userInput != password) {
+                        throw new IllegalStateException("Incorrect Password! " + userInput);
 
+                    }
 
+                    switch (userInput) {
+                        case '3' -> {
 
-                System.out.println("Please provide your username and password to access your account.");
+                            System.out.print("""
+                                    3  ->  Create a new diary entry
+                                                                                                                            
+                                    Enter title:
+                                    """);
+                            String title = input.nextLine();
 
-                diary = new Diary("thetosinfaith","4040");
-            }
+                            System.out.print("Enter body: ");
+                            String body = input.nextLine();
 
+                            diary.createEntry(title, body);
+                            break;
 
-        }
+                        }
 
+                        switch (userInput) {
+                            case '4' -> {
 
+                                System.out.print("""
+                                        4  ->  Enter entry ID to find:
+                                        """);
+                                String entry = input.nextLine();
 
+                                System.out.println("""
+                                        Entry found:
+                                        Title: [Title of the Found Entry]
+                                        Body: [Body Content of the Found Entry]
+                                        """);
+                                Entry foundEntry = diary.findEntryById(0);
+                                break;
+                            }
 
+                            switch (userInput) {
+                                case '5' -> {
 
+                                    System.out.print("""
+                                            5  ->  Enter entry ID to find:
+                                            """);
+                                    String entry = input.nextLine();
 
+                                    System.out.println("""
+                                            Entry found:
+                                            Title: [Title of the Found Entry]
+                                            Body: [Body Content of the Found Entry]
+                                            """);
+                                    Entry foundEntry = diary.findEntryById(0);
+                                    break;
 
+                                }
 
 
+//
+//                    System.out.println(foundEntry.getBody());
+//
+//                    System.out.println("""
+//                    Entry found:
+//                    Title: [Title of the Found Entry]
+//                    Body: [Body Content of the Found Entry]
+//                    """);
 
 
+                            }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private static Diary diary;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        diary.unlockDiary("4040");
-
-        System.out.println("Create a new diary entry: ");
-
-        String title = "Enter title: ";
-        String body = "Enter body: ";
-
-        diary.createEntry(title, body);
-
-        System.out.println("Enter entry ID to find: ");
-        Entry foundEntry = diary.findEntryById(0);
-
-        System.out.println(foundEntry.getBody());
-
-        System.out.println("""
-                Entry found:
-                Title: [Title of the Found Entry]
-                Body: [Body Content of the Found Entry]
-                """);
-
-    }
-
-}
